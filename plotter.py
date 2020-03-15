a3c44d42c3ea4c9a2c54bfd18ddf2db42d364cfc
mrskoog <askoog89@gmail.com>
2020-03-15 21:58:06 +0100

    Improve prediction with new feature
    - Added one more data point timedelayed 2*30min as knn feature
    - Added ROC curve for evalueation in python script
    - Fixed PWM bug
    - Added 3D scatter plot of feratures

--- plotter.py
+++ plotter.py
@@ -3,52 +3,58 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
+from mpl_toolkits import mplot3d
 
 ALTITUDE_OF_SENSOR = 103
 
-def dump_c_data(pressure, delta, rain):
+def dump_c_data(pressure,pressure1, delta, rain):
     with open("dataset.h", "w") as f:
         test = len(pressure)
         f.write("#define SIZE_DATASET "+str(test)+"\n")
-        f.write("const float dataset[SIZE_DATASET][2] = { \n")
+        f.write("const float dataset[SIZE_DATASET][3] = { \n")
         for i in range(0, len(pressure)-1):
-            f.write("{ "+ str(pressure[i]) +", " + str(delta[i]) + "},\n")
-        f.write("{ "+ str(pressure[i]) +", " + str(delta[i]) + "}\n")
+            f.write("{ "+ str(pressure[i]) +", "+ str(pressure1[i]) +", " +  str(delta[i]) + "},\n")
+        f.write("{ "+ str(pressure[i]) +", "+ str(pressure1[i]) +", " + str(delta[i]) + "}\n")
         f.write("};\n")
 
         f.write("const byte data_tag[SIZE_DATASET] = { \n")
         for i in range(0, len(pressure)-1):
-            f.write(str(rain[i])+",\n")
-        f.write(str(rain[i])+"};\n")
+            f.write(str(int(rain[i]))+",\n")
+        f.write(str(int(rain[i]))+"};\n")
 
 data = pd.read_csv('weather_data.csv', names = ['rain','temp','pressure','date'])
 
 time_shifted_p = np.array(data.pressure[0:-2])
 time_shifted_p = time_shifted_p / pow(1.0 - ALTITUDE_OF_SENSOR/44330.0, 5.255) #make pressure sealevel
-time_shifted_r = np.array(data.rain[2:])
+time_shifted_p1 = time_shifted_p[2:]
+time_shifted_p = time_shifted_p[:-2]
+
+time_shifted_r = np.array(data.rain[2:-2])
 
 plt.plot(range(len(time_shifted_p)), time_shifted_p, range(len(time_shifted_r)), (time_shifted_r*1000) + 98000)
 
 delta = np.diff(time_shifted_p) / 2
 delta = np.insert(delta,0,0)
 
-data2 = {'pressure' :  time_shifted_p, 'delta' : delta }
+data2 = {'pressure' :  time_shifted_p,'pressure1' :  time_shifted_p1, 'delta' : delta }
 
 X = pd.DataFrame(data2)
 
 print(X)
 
-fig = plt.figure("scatter")
-sc = plt.scatter(X.pressure, X.delta ,c = time_shifted_r)
+fig = plt.figure("scatter, delta")
+ax = fig.add_subplot(111, projection='3d')
+sc = plt.scatter(X.pressure,X.pressure1, X.delta ,c = time_shifted_r)
 plt.colorbar(sc)
 
 y = time_shifted_r
 
-dump_c_data(time_shifted_p,delta,time_shifted_r)
+dump_c_data(time_shifted_p, time_shifted_p1, delta, time_shifted_r)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
@@ -77,7 +83,7 @@ plt.legend()
 plt.xlabel('n_neighbors')
 plt.ylabel('Accuracy')
 
-# LogisticRegression
+LogisticRegression
 reg = LogisticRegression(solver='sag', max_iter=100)
 reg.fit(X_train, y_train)
 train_accuracy = reg.score(X_train, y_train)
@@ -92,5 +98,16 @@ train_accuracy = RF.score(X_train, y_train)
 test_accuracy = RF.score(X_test, y_test)
 print("RF train_accuracy",train_accuracy)
 print("RF test_accuracy",test_accuracy)
+
+knn = KNeighborsClassifier(n_neighbors=13, metric='euclidean',algorithm='brute')
+knn.fit(X_train, y_train)
+
+plt.figure("knn ROC curve")
+y_pred_proba = knn.predict_proba(X_test)[::,1]
+fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba)
+auc = metrics.roc_auc_score(y_test, y_pred_proba)
+plt.plot(fpr,tpr,label="data 1, auc="+str(auc))
+plt.legend(loc=4)
+
 
 plt.show()
--- rainBrain/rainBrain.ino
+++ rainBrain/rainBrain.ino
@@ -4,17 +4,18 @@
 #include "dataset.h"
 #define LED_PIN_WHITE 4
 #define LED_PIN_BLUE 6
-#define K_NEIGHBORS 9
+#define K_NEIGHBORS 13
 #define MILLISECONDS_30MIN 1800000
 #define WHITE_PWN 90
+#define ALTITUDE 103
 
 
 Adafruit_BMP085 bmp;
 short altitude = 0;
 
-float euclidean_distance_2d(float x0, float x1, float y0, float y1)
+float euclidean_distance_2d(float x0, float x1, float y0, float y1, float z0, float z1)
 {
-  return sqrt(pow(x1 - x0, 2) + pow(y1 - y0, 2));
+  return sqrt(pow(x1 - x0, 2) + pow(y1 - y0, 2) + pow(z1 - z0, 2));
 }
 
 float calc_presure_delta(float pressure, float previous_pressure)
@@ -54,21 +55,21 @@ void sort_arrays(float *distance_arr, byte *tag_arr)
   }
 }
 
-byte run_knn(float pressure, float delta)
+byte run_knn(float pressure, float previous_pressure,  float delta)
 {
   float neighbors_distance[K_NEIGHBORS] = {};
   byte neighbors_tag[K_NEIGHBORS] = {};
 
   //fill arrays with some data
   for (byte i = 0; i < K_NEIGHBORS; ++i) {
-    neighbors_distance[i] = euclidean_distance_2d(pressure, dataset[i][0], delta, dataset[i][1]);
+    neighbors_distance[i] = euclidean_distance_2d(pressure, dataset[i][0], previous_pressure, dataset[i][1], delta, dataset[i][2]);
     neighbors_tag[i] = data_tag[i];
   }
   sort_arrays(neighbors_distance, neighbors_tag);
 
   // calculate the rest of the distances and find K smallest distances
   for (int i = K_NEIGHBORS; i < SIZE_DATASET; ++i) {
-    float dist =  euclidean_distance_2d(pressure, dataset[i][0], delta, dataset[i][1]);
+    float dist =  euclidean_distance_2d(pressure, dataset[i][0], previous_pressure, dataset[i][1], delta, dataset[i][2]);
     for (byte j = 0; j < K_NEIGHBORS; j--) {
       if (dist <= neighbors_distance[j]) {
         neighbors_distance[K_NEIGHBORS-1] = dist;
@@ -86,41 +87,23 @@ void setup() {
   digitalWrite(LED_PIN_BLUE, LOW);
   pinMode(LED_PIN_WHITE, OUTPUT);
   analogWrite(LED_PIN_WHITE, 255 - WHITE_PWN);
-  EEPROM.get(0, altitude);   
   Serial.begin(9600);
   delay(1000);
 
-  Serial.print("Current altitude set to:");
-  Serial.println(altitude);
-  Serial.print("Set new altitude: ");
-
-  int timeout = 1000;
-  while (Serial.available() == 0) {
-     delay(10);
-     if(--timeout == 0) {
-      break;
-     }
-  }
-  if(timeout > 0) {
-    altitude = Serial.parseFloat();
-    EEPROM.put(0, altitude); 
-    Serial.println("\nNew altitude saved");
-  }
-  
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP185 sensor, check wiring!");
     while (1) {}
   }
 }
 
-
 void loop() {
   float pressure = 0;
   float delta = 0;
   static float previous_pressure = 0;
+  static float previous2_pressure = 0;
 
   for (size_t i = 0; i < 5; i++) {
-    pressure += (float)bmp.readSealevelPressure(altitude);
+    pressure += (float)bmp.readSealevelPressure(ALTITUDE);
   }
   pressure = pressure / 5;
 
@@ -129,11 +112,12 @@ void loop() {
     Serial.println(pressure);
 
     delta = calc_presure_delta(pressure, previous_pressure);
+    previous2_pressure = previous_pressure;
     previous_pressure = pressure;
-    byte rain = run_knn(pressure, delta);
+    byte rain = run_knn(pressure, previous2_pressure, delta);
     if(rain) {
       digitalWrite(LED_PIN_BLUE, LOW);
-      digitalWrite(LED_PIN_WHITE, HIGH);
+      analogWrite(LED_PIN_WHITE, 256);
     } else {
       digitalWrite(LED_PIN_BLUE, HIGH);
       analogWrite(LED_PIN_WHITE, 255 - WHITE_PWN);
